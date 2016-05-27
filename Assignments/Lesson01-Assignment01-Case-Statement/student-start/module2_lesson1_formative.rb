9a0448660eabace58c1ff585ae90c9bf210ddf12
Benoit de Chateauvieux <benoit@HPVN-0045L.local>
2016-05-27 17:31:45 +0700

    implement module2_lesson1 and module2_lesson2

--- Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
+++ Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
@@ -5,5 +5,14 @@ case
   when some_var == "pink elephant" then puts "Don't think about the pink elephant!"
   when another_var.nil? then puts "Question mark in the method name?"
   when some_var == false then puts "Looks like this one should execute"
-  else puts "I guess nothing matched... But why?"
+  else 	puts "I guess nothing matched... But why?"
 end
+
+%Q{Benoit 
+Momo
+\n
+Les copains
+}.lines do |line|
+	puts "Line: #{line}"
+end
+puts " Pouet".lstrip!
